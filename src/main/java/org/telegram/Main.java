2a37d25bfb905895ba959b42b1fa491543a01213
LucasCabello <lmcabellog@gmail.com>
2017-06-14 22:19:15 -0400

    Added artifact and fixes

---
+++ src/main/java/org/telegram/Main.java
@@ -0,0 +1,18 @@
+package org.telegram;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        try {
+            botsApi.registerBot(new JeopardyBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
