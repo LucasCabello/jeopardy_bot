e91748f1dc2133715d00f1ea950e5afee0e1085f
LucasCabello <lmcabellog@gmail.com>
2017-06-14 21:45:34 -0400

    Echo bot

---
+++ src/main/java/org/telegram/JeopardyBot.java
@@ -0,0 +1,32 @@
+package org.telegram;
+
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.api.methods.send.*;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class JeopardyBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                sendMessage(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "ducks_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return System.getenv("DUCKS_TOKEN");
+    }
+}
